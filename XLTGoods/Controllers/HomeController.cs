e6199f1e3bbf33a23deea37b8ab39a11cffea57a
huangpbj <13552299260@163.com>
2018-03-13 16:43:25 +0800

    显示编辑分视图

--- XLTGoods/Controllers/HomeController.cs
+++ XLTGoods/Controllers/HomeController.cs
@@ -22,5 +22,85 @@ namespace XLTGoods.Controllers
                 return Json(new { data = property }, JsonRequestBehavior.AllowGet);
             }
         }
+        [HttpGet]
+        public ActionResult Save(int id)
+        {
+            using (XLTDatabaseEntities dc = new XLTDatabaseEntities())
+            {
+                var v = dc.Property.Where(a => a.ProId == id).FirstOrDefault();
+                return View(v);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Save(Property prop)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (XLTDatabaseEntities dc = new XLTDatabaseEntities())
+                {
+                    if (prop.ProId > 0)
+                    {
+                        //Edit 
+                        var v = dc.Property.Where(a => a.ProId == prop.ProId).FirstOrDefault();
+                        if (v != null)
+                        {
+                            v.ProId = prop.ProId;
+                            v.PName = prop.PName;
+                            v.Brand = prop.Brand;
+                            v.Number = prop.Number;
+                            v.Model = prop.Model;
+                            v.OneCategory = prop.OneCategory;
+                            v.TwoCategory = prop.TwoCategory;
+                           
+                        }
+                    }
+                    else
+                    {
+                        //Save
+                        dc.Property.Add(prop);
+                    }
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            using (XLTDatabaseEntities dc = new XLTDatabaseEntities())
+            {
+                var v = dc.Property.Where(a => a.ProId == id).FirstOrDefault();
+                if (v != null)
+                {
+                    return View(v);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteProperty(int id)
+        {
+            bool status = false;
+            using (XLTDatabaseEntities dc = new XLTDatabaseEntities())
+            {
+                var v = dc.Property.Where(a => a.ProId == id).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Property.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
     }
 }
---
+++ XLTGoods/Models/Extended/Property.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace XLTGoods.Models
+{
+    [MetadataType(typeof(PropertyMetadata))]
+    public partial class Property
+    {
+
+    }
+
+
+     public class PropertyMetadata
+        {
+            [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide PName")]
+            public string PName { get; set; }
+        }
+}
